87bd0ca2771cd8cae3958e37d81c26f0e42c796c
NishaDhakate <nishadhakate1997@gmail.com>
2026-06-10 22:53:38 +0530

    print statement added

--- src/test/java/API_demo_day1/Day_01.java
+++ src/test/java/API_demo_day1/Day_01.java
@@ -1,4 +1,14 @@
 package API_demo_day1;
 
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+
 public class Day_01 {
+
+    
+    @Test
+    public void helthchecktest(){
+        given().when().get("https://restful-booker.herokuapp.com/").then().assertThat().statusCode(200);
+    }
 }
--- src/test/java/API_demo_day1/Day_02.java
+++ src/test/java/API_demo_day1/Day_02.java
@@ -1,4 +1,31 @@
 package API_demo_day1;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import java.io.File;
+import java.io.IOException;
+
 public class Day_02 {
+
+    public static void main(String[] args) throws IOException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testautomationpractice.blogspot.com/");
+        driver.manage().window().maximize();
+
+        //To take screenshot
+      /*  File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File destination = new File("D:\\TestData\\Screensot\\example.png");
+        FileUtils.copyFile(screenshot, destination);*/
+
+        //To object of screenshot
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File sourcefile = ts.getScreenshotAs(OutputType.FILE);
+        File destination = new File("D:\\TestData\\Screensot\\Nisha.png");
+        FileUtils.copyFile(sourcefile, destination); // ✅ Copy step added
+    }
 }
